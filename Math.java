39dcd554c21a4cf8125e4129a5844c6bc944f496
Mikhail <alllkotraz@gmail.com>
2022-06-18 16:30:31 +0600

    C0. Initial commit. Main function

---
+++ Math.java
@@ -0,0 +1,9 @@
+public class Math {
+	public static void main(String[] args) {
+		Integer a = Integer.valueOf(args[0]);
+		String action = args[1];
+		Integer b = Integer.valueOf(args[2]);
+	}
+}
+
+
