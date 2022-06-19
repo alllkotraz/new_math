08764d5f9f81dcb49f50a9c9afec90caaedfb7f3
alllkotraz <51913451+alllkotraz@users.noreply.github.com>
2022-06-19 15:24:05 +0500

    Update Math.java

--- Math.java
+++ Math.java
@@ -19,10 +19,10 @@ public class Math {
 	}
 
 	public static Integer subtraction (Integer a, Integer b) {
-	       return a - b;
+	       return a * b;
 	}	     
 	
 	public static Integer addition (Integer a, Integer b) {
-		return a + b;
+		return a - b;
 	}	
 }
